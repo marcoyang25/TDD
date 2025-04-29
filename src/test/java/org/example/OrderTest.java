a76a5056d9a3d46d78f0195865bfac902f2731ec
Marco <marco831010@gmail.com>
2025-04-29 17:52:01 +0900

    verify mock is only called once

--- src/test/java/org/example/OrderTest.java
+++ src/test/java/org/example/OrderTest.java
@@ -12,13 +12,14 @@ class OrderTest {
         Order order = new Order(mockEmailUtil, new Weather());
         order.insertOrder("marco@gmail.com", 1, 100);
         assertEquals("marco@gmail.com", mockEmailUtil.getReceiveEmail());
+        mockEmailUtil.verifyCalledOnce();
     }
 
 }
 
 class MockEmailUtil implements IEmailUtil {
-
     String receiveEmail;
+    int callCount = 0;
 
     public String getReceiveEmail() {
         return receiveEmail;
@@ -26,6 +27,17 @@ class MockEmailUtil implements IEmailUtil {
 
     @Override
     public void sendCustomer(String email) {
+        callCount++;
         receiveEmail = email;
+    }
+
+    public void verifyCalledOnce() {
+        if (callCount != 1) {
+            throw new AssertionError("Expected sendCustomer to be called exactly once, but was called " + callCount + " times");
+        }
+    }
+
+    public int getCallCount() {
+        return callCount;
     }
 }
